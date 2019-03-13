bb9219321af8648fbbf4e51e618a4b6ec2ee393a
ArtySad <artemoner@mail.ru>
2019-03-13 13:54:22 +0300

    /

--- homeworks/homework_01/hw1_arrsearch.py
+++ homeworks/homework_01/hw1_arrsearch.py
@@ -5,11 +5,11 @@
 def find_indices(input_list, n):    
     i = 0
     j = len(input_list)-1
-    while i!=j:
+    while i !=j :
         if input_list[i] + input_list[j] == n:
-            return (i, j)
+            return i, j
         elif input_list[i] + input_list[j] < n:
-           i=i+1
-        elif  input_list[i] + input_list[j] > n: 
-           j=j-1        
+           i = i+1
+        elif input_list[i] + input_list[j] > n:
+           j = j-1
     return
